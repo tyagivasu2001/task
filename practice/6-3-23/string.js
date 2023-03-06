829889d97cf5b30dbfb7d4e0067bb3cc31a34fff
tyagivasu2001 <vasutyagi2001@gmail.com>
2023-03-06 18:54:52 +0530

    commit

--- practice/6-3-23/string.js
+++ practice/6-3-23/string.js
@@ -1,30 +1,27 @@
-var arr=[20,2,22,52,2,11,5,2,4];
-let i=0;
-let j=arr.length-1;
-for(let k=0;k<arr.length;k++){
-    if(arr[i]==2){
-        if(arr[j]==2){
-            j--;
-        }
-        else{
-            let temp=arr[j];
-            arr[j]=arr[i];
-            arr[i]=temp;
-        }
+var arr = [20, 2, 22, 52, 2, 11, 5, 2, 4];
+let i = 0;
+let j = arr.length - 1;
+for (let k = 0; k < arr.length; k++) {
+  if (arr[i] == 2) {
+    if (arr[j] == 2) {
+      j--;
+    } else {
+      let temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
     }
-    else{
-        i++;
-    }
-
+  } else {
+    i++;
+  }
 }
 console.log(arr);
-for(let a=0;a<arr.length;a++){
-    for( let b=0;b<arr.length;b++){
-     if(arr[i]>arr[j]){
-        let temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-     }
+for (let a = 0; a < arr.length; a++) {
+  for (let b = a + 1; b < arr.length; b++) {
+    if (arr[a] > arr[b]) {
+      let temp = arr[a];
+      arr[a] = arr[b];
+      arr[b] = temp;
     }
+  }
 }
-
+console.log(arr);
