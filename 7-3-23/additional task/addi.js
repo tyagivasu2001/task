9e92c56aa423488e36c195d57ff0f49d753868a1
tyagivasu2001 <vasutyagi2001@gmail.com>
2023-03-10 10:51:49 +0530

    commit

--- 7-3-23/additional task/addi.js
+++ 7-3-23/additional task/addi.js
@@ -1,17 +1,23 @@
 let source = document.getElementById("selectSource");
 let destination = document.getElementById("selectDestination");
 
-let route = new Map();
-route.set("Delhi", ["Delhi"]);
-route.set("Mumbai", ["Mumbai"]);
-route.set("Banglore", ["Banglore"]);
-route.set("Pune", ["Pune"]);
-route.set("Noida", ["Noida"]);
+let sourceroute = new Map();
+sourceroute.set("Delhi", ["Delhi"]);
+sourceroute.set("Mumbai", ["Mumbai"]);
+sourceroute.set("Banglore", ["Banglore"]);
+sourceroute.set("Pune", ["Pune"]);
+sourceroute.set("Noida", ["Noida"]);
+let destinatinroute= new Map();
+destinatinroute.set("Delhi", ["Delhi"]);
+destinatinroute.set("Mumbai", ["Mumbai"]);
+destinatinroute.set("Banglore", ["Banglore"]);
+destinatinroute.set("Pune", ["Pune"]);
+destinatinroute.set("Noida", ["Noida"]);
+
 
 function src() {
   let destOptions = destination.options;
-  let blockedDest = route.get(source.value);
-  console.log(destOptions[2].value);
+  let blockedDest = sourceroute.get(source.value);
 
   for (let i = 0; i < destOptions.length; i++) {
     destOptions[i].disabled = false;
@@ -26,14 +32,41 @@ function src() {
   }
 }
 
+function desti() {
+  let srcOptions = source.options;
+  let blockedsrc = destinatinroute.get(destination.value);
+  for (let i = 0; i < srcOptions.length; i++) {
+    srcOptions[i].disabled = false;
+  }
+
+  for (let i = 1; i < srcOptions.length; i++) {
+    for (let j = 0; j < blockedsrc.length; j++) {
+      if (srcOptions[i].value == blockedsrc[j]) {
+        srcOptions[i].disabled = true;
+      }
+    }
+  }
+}
 function valid() {
-  let newBlockedDest = route.get(source.value);
+  let newBlockedDest = sourceroute.get(source.value);
   newBlockedDest.push(destination.value);
-  route.set(source.value, newBlockedDest);
+  sourceroute.set(source.value, newBlockedDest);
+
+  let newroutefordestination=destinatinroute.get(destination.value);
+  newroutefordestination.push(source.value);
+  destinatinroute.set(destination.value,newroutefordestination);
 
   let showRoutes = document.getElementById("routes");
   showRoutes.innerHTML += source.value + " to " + destination.value + "<br>";
 
   source.selectedIndex = 0;
   destination.selectedIndex = 0;
+  let destOptions = destination.options;
+  let srcOptions = source.options;
+  for (let i = 0; i < srcOptions.length; i++) {
+    srcOptions[i].disabled = false;
+  }
+  for (let i = 0; i < destOptions.length; i++) {
+    destOptions[i].disabled = false;
+  }
 }
