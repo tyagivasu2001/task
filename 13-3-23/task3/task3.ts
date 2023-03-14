a6e0104424eea78d8e946006aec5f01e2c28fa5d
tyagivasu2001 <vasutyagi2001@gmail.com>
2023-03-14 10:48:40 +0530

    commit

--- 13-3-23/task3/task3.ts
+++ 13-3-23/task3/task3.ts
@@ -1,23 +1,27 @@
-var arr:any=['1', 'a', '2', 'b', '3', 'c', '4', 'd', '5', 'e'];
-for(let i=0;i<arr.length;i++)
-{
-    try{
-        // used try catch because we can't parse a charecter to number
-        if(Number.isInteger(parseInt(arr[i])))
-        {
-            //change the value of string type to number
-            arr[i]=parseInt(arr[i]);
-        
-        }
-        else
-        {
-            console.log(arr[i].toUpperCase());
-        }
+/***************************************************** */
+var arr= ["1", "a", "2", "b", "3", "c", "4", "d", "5", "e"];
+
+var numberArray=[0];
+
+for (let i = 0; i < arr.length; i++) {
+
+  try {
+
+    // used try catch because we can't parse a charecter to number
+    if (Number.isInteger(parseInt(arr[i])))
+     {
+      //change the value of string type to number
+      numberArray[i] = parseInt(arr[i]);
     }
-    catch(e)
-    {
-        console.log(e);
+     else 
+     {
+      console.log(arr[i].toUpperCase());
     }
+  }
+   catch (e) 
+   {
+    console.log(e);
+  }
 }
 // print the modified array
-console.log(arr);
+console.log(numberArray);
