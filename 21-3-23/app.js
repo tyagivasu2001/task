92963367277f2a9d89bf4d239ca09bdd93f6bfe7
tyagivasu20012 <gt352733@gmail.com>
2023-03-23 09:25:04 +0530

    commit

--- 21-3-23/app.js
+++ 21-3-23/app.js
@@ -1,22 +1,15 @@
 const express =require('express');
 const app=express();
-const path = require('path');
-const { MongoClient } = require('mongodb');
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
 app.use(express.static('public'));
+const router = require('./routes/tablebooking');
 
 
-app.get('/',(req,res)=>{
+app.use('/', router);
 
-})
-app.get('/book',(req,res)=>{
-    
-})
 
 
 app.listen(3000,()=>{
-     client.connect();
-    let database =client.db('Restaurent').createCollection('Tablebooking');
+    //  client.connect();
+    // let database =client.db('Restaurent').createCollection('Tablebooking');
     console.log("Node is running on port 3000!!")
 })
--- 21-3-23/public/new.js
+++ 21-3-23/public/new.js
@@ -1,77 +1,129 @@
-localStorage.clear(); 
+localStorage.clear();
 
-function book(){
-    let date=document.getElementById("date").value;
-    let time=document.getElementById("timebook").value;
-    let seat=document.getElementById("forseat").value;
-    if(date==""){
-         alert("Please Select Date");
+
+function book() {
+    const obj = {
+        date: String,
+        time: String,
+        seat: String
     }
-    else{
+    let date = document.getElementById("date").value;
+    let time = document.getElementById("timebook").value;
+    let seat = document.getElementById("forseat").value;
+    if (date == "") {
+        alert("Please Select Date");
+    }
+    else {
 
-    
-    let  key=date +seat;
-    let val=localStorage.getItem(key);
-    if(val!=null){
-        let arr=val.split('#');
-        if(arr.indexOf(time)>=0){
-            alert("Seat Full");
-            return;
+
+        let key = date + seat;
+        let val = localStorage.getItem(key);
+        let flag = true;
+        obj.date = date;
+        obj.time = time;
+        obj.seat = seat;
+
+
+        async function checkTable() {
+
+
+            const url = `http://localhost:3000/find/`;
+            const response = await fetch(url + JSON.stringify(obj), {
+                method: 'POST',
+                headers: {
+                    "Content-Type": 'application/json'
+                }
+            });
+            console.log(response);
         }
-        else{
-            val += time+'#';
 
-
+        if (val != null) {
+            let arr = val.split('#');
+            if (arr.indexOf(time) >= 0) {
+                alert("Seat Full");
+                return;
+            }
+            else {
+                val += time + '#';
+                //----------------------------------Insert Call
+                setData(obj);
+            }
         }
-        
+        else {
+            val = time + '#';
+            //------------------------------------Insert Call
+            setData(obj);
+        }
+        localStorage.setItem(key, val);
+        display(date, time, seat);
     }
-    else{
-        val=time+'#';
-        
-    }
-    localStorage.setItem(key,val);
-    display(date,time,seat);
 }
-}
-function display(date,time,seat){
+function display(date, time, seat) {
     const para = document.createElement("div");
-    para.setAttribute("id",date+time+seat);
+    para.setAttribute("id", date + time + seat);
     const line = document.createElement("p");
-    line.innerHTML=seat +": is  Booked  for: "+date+" Between slot: "+time;
+    line.innerHTML = seat + ": is  Booked  for: " + date + " Between slot: " + time;
     para.appendChild(line);
-    const button=document.createElement("button");
-    button.innerHTML="Delete";
-    button.setAttribute("onclick","Delete("+JSON.stringify(date)+","+JSON.stringify(time)+","+JSON.stringify(seat)+")");
+    const button = document.createElement("button");
+    button.innerHTML = "Delete";
+    button.setAttribute("onclick", "Delete(" + JSON.stringify(date) + "," + JSON.stringify(time) + "," + JSON.stringify(seat) + ")");
     para.appendChild(button);
-    let x=document.getElementById("display");
+    let x = document.getElementById("display");
     x.appendChild(para);
-    
-
-
 }
 
-function Delete(date,time,seat){
-    let key=date+seat;
-    let value=localStorage.getItem(key);
-    let items=value.split('#');
-    let index=items.indexOf(time);
-    items.splice(index,1);
-    let str="";
-    for(let i=0;i<items.length;i++){
-         str+=items[i]+'#';
+function Delete(date, time, seat) {
+    const obj = {
+        date: String,
+        time: String,
+        seat: String
     }
-    localStorage.setItem(key,str);
-    let id=date+time+seat;
+    let key = date + seat;
+    let value = localStorage.getItem(key);
+    let items = value.split('#');
+    let index = items.indexOf(time);
+    items.splice(index, 1);
+    let str = "";
+    for (let i = 0; i < items.length; i++) {
+        str += items[i] + '#';
+    }
+    localStorage.setItem(key, str);
+    let id = date + time + seat;
     document.getElementById(id).remove();
-    
-
-
+    obj.date = date;
+    obj.time = time;
+    obj.seat = seat;
+    //-----------------------------------------Delete Call
+    deleteData(obj);
 }
-function datevalidate(){
-    let todaydate=document.getElementById("date");
-    let date =new Date();
-    let str=date.toISOString();
-    todaydate.setAttribute("min",str.substr(0,10));
+function datevalidate() {
+    let todaydate = document.getElementById("date");
+    let date = new Date();
+    let str = date.toISOString();
+    todaydate.setAttribute("min", str.substr(0, 10));
     console.log(str);
 
+}
+
+//------------------------------------------------Insert Method
+async function setData(obj) {
+    const url = `http://localhost:3000/setData/`;
+
+    await fetch(url + JSON.stringify(obj), {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+//----------------------------------------------------Delete method 
+async function deleteData(obj) {
+    const url = `http://localhost:3000/deleteData/`;
+    await fetch(url + JSON.stringify(obj), {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
 }
